50565e3c6185bf5b1c3f43c53ba57c431e565c13
Rafael Evangelista <rafael.evans@live.com>
2025-11-02 14:52:37 -0300

    GoLang foundations - First steps loops

---
+++ foundation/loop.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// for i := 0; i <= 10; i++ {
+	// 	println(i)
+	// }
+
+	fruits := []string{"apple", "orange", "mango"}
+	for k, v := range fruits {
+		println(k, v)
+	}
+
+	// i := 0
+
+	// for i < 9 {
+	// 	println(i)
+	// 	i++
+	// }
+}
